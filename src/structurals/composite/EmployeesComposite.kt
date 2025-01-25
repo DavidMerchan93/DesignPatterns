37e1d9028fbd0a91ae8806dcdb038e1c3f18dab1
David Merchan <davidmerchan10@gmail.com>
2025-01-24 19:21:50 -0500

    Implementation of Composite pattern

---
+++ src/structurals/composite/EmployeesComposite.kt
@@ -0,0 +1,83 @@
+package structurals.composite
+
+interface Employee {
+    fun getEmployeeCount(): Int
+    fun getEmployeeName(): String
+}
+
+class EmployeeLeaf(val name: String) : Employee {
+    override fun getEmployeeCount(): Int {
+        return 1
+    }
+
+    override fun getEmployeeName(): String {
+        return name
+    }
+}
+
+class Manager(val name: String): Employee {
+
+    private val employees = mutableListOf<Employee>()
+
+    override fun getEmployeeCount(): Int {
+        return employees.sumOf { it.getEmployeeCount() }
+    }
+
+    override fun getEmployeeName(): String {
+        return buildString {
+            append(name)
+            employees.forEach {
+                append("\n  \n")
+                append(it.getEmployeeName().replace("\n","\n   "))
+            }
+        }
+    }
+
+    fun findLastLeaf(): Employee? {
+        if (employees.isEmpty())
+            return null
+
+        val last = employees.last()
+
+        if (last is Manager)
+            return last.findLastLeaf()
+
+        return last
+    }
+
+    fun addEmployee(employee: Employee) {
+        employees.add(employee)
+    }
+}
+
+fun main() {
+    val ceo = Manager("CEO")
+    val vpMarketing = Manager("VP of Marketing")
+    val marketingSpecialist1 = EmployeeLeaf("Marketing Specialist 1")
+    val marketingSpecialist2 = EmployeeLeaf("Marketing Specialist 2")
+
+    val vpEngineering = Manager("VP of Engineering")
+    val softwareEngineer1 = EmployeeLeaf("Software Engineer 1")
+    val softwareEngineer2 = EmployeeLeaf("Software Engineer 2")
+
+    val engineeringManager = Manager("Engineer Manager")
+    val qaEngineer = EmployeeLeaf("QA Engineer")
+
+    engineeringManager.addEmployee(qaEngineer)
+
+    vpEngineering.addEmployee(softwareEngineer1)
+    vpEngineering.addEmployee(softwareEngineer2)
+    vpEngineering.addEmployee(engineeringManager)
+
+    vpMarketing.addEmployee(marketingSpecialist1)
+    vpMarketing.addEmployee(marketingSpecialist2)
+
+    ceo.addEmployee(vpMarketing)
+    ceo.addEmployee(vpEngineering)
+
+    println("-------------- All employees ---------------")
+    println(ceo.getEmployeeName())
+
+    println("-------------- Last employee ---------------")
+    println(ceo.findLastLeaf()?.getEmployeeName())
+}
